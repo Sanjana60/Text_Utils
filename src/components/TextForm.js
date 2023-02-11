2faae1cae005bd8eb290f205ed323955a73a0611
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-11 17:18:11 +0530

    text form

---
+++ src/components/TextForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TextForm(props) {
+  return (
+    <div>
+<h1>{props.heading}</h1>
+  <div class="mb-3">
+    <textarea class="form-control" id="myBox" rows="14"></textarea>
+  </div></div>
+  )
+}

0151242e307aaff405cc817021d7275eef446615
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-12 18:24:06 +0530

    dark and light mode success alert messages

--- src/App.js
+++ src/App.js
@@ -8,20 +8,33 @@ import Alert from './components/Alert';
 function App() {
 
   const [mode, setmode] = useState('light');
+  const [alert, setalert] = useState(null);
+
+  const showalert =(message,type) =>{
+    setalert({
+      msg:message,
+      ty:type}
+      
+      )
+    
+
+  }
   const tooglemode =()=>{
     if(mode === 'light'){
       setmode('dark');
       document.body.style.backgroundColor = '#042743';
+      showalert("Dark Mode has been enabled","Success");
     }
     else{
       setmode('light');
       document.body.style.backgroundColor = 'white';
+      showalert("Light Mode has been enabled","Success");
     }
   }
   return (
     <>
     <NavBar title ="TextUtils" mode={mode} tooglemode={tooglemode} about ="AboutUs"/>
-    <Alert alert="This is Alert"/>
+    <Alert alert={alert}/>
     <div className="container my-3">
     <TextForm heading = "Enter the text to Analyze" mode={mode}/>
     {/* <About/> */}

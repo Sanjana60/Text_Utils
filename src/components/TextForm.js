b9fa874112e51510ef0fd257f80d88675a506fe7
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-12 19:16:55 +0530

    word length corrected and text area color updated

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,7 +31,7 @@ export default function TextForm(props) {
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text}
-                    onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#042743':'white'}} id="myBox" rows="10"></textarea>
+                    onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'pink':'white' }} id="myBox" rows="10"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
             <button className="btn btn-primary" onClick={handleUpClicklow}>Convert to lowercase</button>
@@ -40,7 +40,7 @@ export default function TextForm(props) {
 
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>Your text summary</h1>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{text.split(" ").filter((t) => t!=="").length} words and {text.length} characters</p>
             <p>{0.08 * text.split(" ").length} Minutes Read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:'Enter something in the text box to preview here.'}</p>
